83bc1a8f329080760e9cca55fc42a159272f4310
deejeon <61816883+deejeon@users.noreply.github.com>
2020-06-05 21:53:26 -0700

    added ObjectMaster

---
+++ ObjectMaster/src/com/danieljeon/objectmaster/HumanTest.java
@@ -0,0 +1,13 @@
+package com.danieljeon.objectmaster;
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human human1 = new Human();
+		Human human2 = new Human();
+		
+		human1.attack(human2);
+	}
+
+}
